ea49d69a46c248600a31127a863dd2168fef2479
trickertite <trickertite@gmail.com>
2015-03-04 21:46:44 +0200

    CRUD operations on Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,46 @@
 class PostsController < ApplicationController
 	def index
-		
+		@posts = Post.all
 	end
+
+	def new
+		@post= Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		if @post = Post.create(post_params)
+			redirect_to posts_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			redirect_to post_path(@post)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
