886d31873ce6007668de7c7e7b502596b8dd18d5
trickertite <trickertite@gmail.com>
2015-03-08 13:49:06 +0200

    devise authentication settled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308084837) do
+ActiveRecord::Schema.define(version: 20150308113439) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150308084837) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
